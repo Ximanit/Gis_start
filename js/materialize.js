1362e71a3f3c7530584af648773190c995b0aa47
Ximanitt <Maximka.kotic@yandex.ru>
2023-04-14 18:49:20 +1000

    ---

--- js/materialize.js
+++ js/materialize.js
@@ -1,13 +1,13 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   var elems = document.querySelectorAll(".sidenav");
-//   var instances = M.Sidenav.init(elems, options);
-// });
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".sidenav");
+  var instances = M.Sidenav.init(elems, options);
+});
 
-// // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
-// // var collapsibleElem = document.querySelector('.collapsible');
-// // var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
+// Initialize collapsible (uncomment the lines below if you use the dropdown variation)
+// var collapsibleElem = document.querySelector('.collapsible');
+// var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
 
-// // Or with jQuery
+// Or with jQuery
 
 // $(document).ready(function () {
 //   $(".sidenav").sidenav();
